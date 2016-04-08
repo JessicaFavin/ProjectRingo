39435eebe3e5f680792f6a338bb06832a720ea13
Jessica Favin <jessica00228@hotmail.fr>
2016-04-08 14:50:42 +0200

    Entity - TCP non bloquant

--- Entity.java
+++ Entity.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.io.*;
 import java.util.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
@@ -10,12 +11,12 @@ public class Entity{
     try{//init les param de l'entity
       int id;
       int input_UDP = 4343;
-      int input_TCP;
+      int input_TCP = 4344;
       InetAddress next_address;
       int next_input_UDP;
-      Inet4Address emergency_address;
       //mulitcast sur l'adresse pour restreindre le message uniquement à la meme adresse
-      int emergency_UDP = 4344;
+      Inet4Address emergency_address;
+      int emergency_UDP = 4345;
       if(args.length!=6 && args.length!=4){
         System.out.println("java Entity port_UDP port_TCP [next_address] [next_port_UDP] emergency_address emergency_UDP");
       }
@@ -42,24 +43,27 @@ public class Entity{
         emergency_address = (Inet4Address) InetAddress.getByName(args[2]);
         emergency_UDP = Integer.parseInt(args[3]);
       } else {
-        System.exit(1);
+        System.out.println("java Entity input_UDP input_TCP");
       }
-      //--------------connexion en udp non bloquante------------------------------
-      // a faire
-      //-------------connexion en tcp non bloquante-------------------------------
-      //a faire
-      //-----connexion en udp au port emercy non bloquante -----------------------
-      //a faire
       Selector sel=Selector.open();
+      //--------------connexion en udp non bloquante----------------------------
       DatagramChannel udp_in=DatagramChannel.open();
-      DatagramChannel udp_emergency=DatagramChannel.open();
       udp_in.configureBlocking(false);
-      udp_emergency.configureBlocking(false);
       udp_in.bind(new InetSocketAddress(input_UDP));
-      udp_emergency.bind(new InetSocketAddress(emergency_UDP));
       udp_in.register(sel,SelectionKey.OP_READ);
+      //-----connexion en udp au port emercy non bloquante ---------------------
+      DatagramChannel udp_emergency=DatagramChannel.open();
+      udp_emergency.configureBlocking(false);
+      udp_emergency.bind(new InetSocketAddress(emergency_UDP));
       udp_emergency.register(sel,SelectionKey.OP_READ);
+      //-------------connexion en tcp non bloquante-----------------------------
+      ServerSocketChannel tcp_in = ServerSocketChannel.open();
+      tcp_in.configureBlocking(false);
+      tcp_in.bind(new InetSocketAddress(input_TCP));
+      tcp_in.register(sel, SelectionKey.OP_ACCEPT);
+
       ByteBuffer buff=ByteBuffer.allocate(100);
+      //---------------boucle en attente d'une action---------------------------
       while(true){
         System.out.println("Waiting for messages");
         sel.select();
@@ -79,13 +83,28 @@ public class Entity{
             String st=new String(buff.array(),0,buff.array().length);
             buff.clear();
             System.out.println("Message :"+st);
-          } /*else if (sk.isReadable() && sk.channel()==tcp_in){
-            System.out.println("Message input_TCP recu");
+          } else if (sk.isAcceptable() && sk.channel()==tcp_in){
+            Socket comSock = (tcp_in.accept()).socket();
+            BufferedReader comBR = new BufferedReader(
+  							new InputStreamReader(
+  									comSock.getInputStream()));
+  					PrintWriter comPW = new PrintWriter(
+  							new OutputStreamWriter(
+  									comSock.getOutputStream()));
+            String st = comBR.readLine();
+            /*
             tcp_in.receive(buff);
             String st=new String(buff.array(),0,buff.array().length);
-            buff.clear();
+            buff.clear();*/
+            System.out.println("Message input_TCP recu");
             System.out.println("Message :"+st);
-          } */else{
+            comPW.println("Message recu!");
+  					comPW.flush();
+            comBR.close();
+  					comPW.close();
+  					comSock.close();
+  					System.out.println("Connection closed.\n");
+          } else{
             System.out.println("Que s'est il passe");
           }
         }
