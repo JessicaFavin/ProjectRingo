e7cbe9e5e26fcd007cd607cff5ce221990d0d247
Jessica Favin <jessica00228@hotmail.fr>
2016-04-08 14:18:27 +0200

    UDP non bloquant

--- Entity.java
+++ Entity.java
@@ -1,47 +1,98 @@
+import java.net.*;
+import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+
 public class Entity{
-  private static  int           next_id = 0;
-  private         int           id;
-  private         int           input_UDP;
-  private         int           input_TCP;
-  private         InetAddress   next_address;
-  private         int           next_input_UDP;
-  private         Inet4Address  emergency_address;
-  //mulitcast sur l'adresse pour restreindre le message uniquement à la meme adresse
-  private         int           emergency_UDP;
+  private static int next_id = 0;
 
   public static void main(String[] args) {
-    if(args.length!=6 && args.length!=4){
-      System.out.println("java Entity port_UDP port_TCP [next_address] [next_port_UDP] emergency_address emergency_UDP");
+    try{//init les param de l'entity
+      int id;
+      int input_UDP = 4343;
+      int input_TCP;
+      InetAddress next_address;
+      int next_input_UDP;
+      Inet4Address emergency_address;
+      //mulitcast sur l'adresse pour restreindre le message uniquement à la meme adresse
+      int emergency_UDP = 4344;
+      if(args.length!=6 && args.length!=4){
+        System.out.println("java Entity port_UDP port_TCP [next_address] [next_port_UDP] emergency_address emergency_UDP");
+      }
+      if(args.length == 6){
+        id = next_id;
+        next_id++;
+        input_UDP = Integer.parseInt(args[0]);
+        input_TCP = Integer.parseInt(args[1]);
+        next_address = InetAddress.getByName(args[2]);
+        next_input_UDP = Integer.parseInt(args[3]);
+        //gestion de la detection de problème plus tard
+        emergency_address = (Inet4Address) InetAddress.getByName(args[4]);
+        emergency_UDP = Integer.parseInt(args[5]);
+      }else if(args.length == 4){
+        id = next_id;
+        next_id++;
+        input_UDP = Integer.parseInt(args[0]);
+        input_TCP = Integer.parseInt(args[1]);
+        //recuperation de self address
+        InetAddress self_address = InetAddress.getByName(Inet4Address.getLocalHost().getHostAddress());//127.000.000.001;
+        next_address = self_address;
+        next_input_UDP = input_UDP;
+        //gestion de la detection de problème plus tard
+        emergency_address = (Inet4Address) InetAddress.getByName(args[2]);
+        emergency_UDP = Integer.parseInt(args[3]);
+      } else {
+        System.exit(1);
+      }
+      //--------------connexion en udp non bloquante------------------------------
+      // a faire
+      //-------------connexion en tcp non bloquante-------------------------------
+      //a faire
+      //-----connexion en udp au port emercy non bloquante -----------------------
+      //a faire
+      Selector sel=Selector.open();
+      DatagramChannel udp_in=DatagramChannel.open();
+      DatagramChannel udp_emergency=DatagramChannel.open();
+      udp_in.configureBlocking(false);
+      udp_emergency.configureBlocking(false);
+      udp_in.bind(new InetSocketAddress(input_UDP));
+      udp_emergency.bind(new InetSocketAddress(emergency_UDP));
+      udp_in.register(sel,SelectionKey.OP_READ);
+      udp_emergency.register(sel,SelectionKey.OP_READ);
+      ByteBuffer buff=ByteBuffer.allocate(100);
+      while(true){
+        System.out.println("Waiting for messages");
+        sel.select();
+        Iterator<SelectionKey> it=sel.selectedKeys().iterator();
+        while(it.hasNext()){
+          SelectionKey sk=it.next();
+          it.remove();
+          if(sk.isReadable() && sk.channel()==udp_in){
+            System.out.println("Message input_UDP recu");
+            udp_in.receive(buff);
+            String st=new String(buff.array(),0,buff.array().length);
+            buff.clear();
+            System.out.println("Message :"+st);
+          } else if (sk.isReadable() && sk.channel()==udp_emergency){
+            System.out.println("Message emergency_UDP recu");
+            udp_emergency.receive(buff);
+            String st=new String(buff.array(),0,buff.array().length);
+            buff.clear();
+            System.out.println("Message :"+st);
+          } /*else if (sk.isReadable() && sk.channel()==tcp_in){
+            System.out.println("Message input_TCP recu");
+            tcp_in.receive(buff);
+            String st=new String(buff.array(),0,buff.array().length);
+            buff.clear();
+            System.out.println("Message :"+st);
+          } */else{
+            System.out.println("Que s'est il passe");
+          }
+        }
+      }
+    } catch (Exception e){
+      System.out.println(e);
+      e.printStackTrace();
     }
-    if(args.length == 6){
-      this.id = next_id;
-      next_id++;
-      this.input_UDP = args[0];
-      this.input_TCP = args[1];
-      this.next_address = args[2];
-      this.next_input_UDP = args[3];
-      //gestion de la detection de problème plus tard
-      this.emergency_address = args[4];
-      this.emergency_UDP = args[5];
-    }
-    if(args.length == 4){
-      this.id = next_id;
-      next_id++;
-      this.input_UDP = args[0];
-      this.input_TCP = args[1];
-      //recuperation de self address
-      InetAddress self_address = 127.000.000.001;
-      this.next_address = self_address;
-      this.next_input_UDP = this.input_UDP;
-      //gestion de la detection de problème plus tard
-      this.emergency_address = args[2];
-      this.emergency_UDP = args[3];
-    }
-    //--------------connexion en udp non bloquante------------------------------
-    // a faire
-    //-------------connexion en tcp non bloquante-------------------------------
-    //a faire
-    //-----connexion en udp au port emercy non bloquante -----------------------
-    //a faire
   }
 }
